bd1d2e23d57b12695ef22b61c6bcf16b51576201
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-25 19:19:02 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var app = express();
 
 // Build Your Route Here
 app.get('/', function(req, resp) {
-<<<<<<< HEAD
+
   resp.render('index', { welcomePhrase: 'Hello, World from Express and EJS!' });
-=======
   const welcomePhrase("Hellow, World from Express and EJS!")
   resp.render('index.ejs');
->>>>>>> 48eaee82e7340e9920e2df5e8e37b0c76b6403c4
+
 });
 
 module.exports = app;
